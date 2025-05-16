54b95fea5f5d9a3a388ea66eb1b2759edbb6d1e0
Jhommuel Pastoral <jhommuelpastoral9@gmail.com>
2025-05-16 21:52:56 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,7 @@ io.on("connection", (socket) => {
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://socialmedia-9d1b.onrender.com",
   credentials: true
 }));
 
